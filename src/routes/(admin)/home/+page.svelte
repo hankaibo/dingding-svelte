ddefc9b7a9da141760f4f04ce60772879f0b914b
hankaibo <hankaibo@hotmail.com>
2023-11-26 22:48:50 +0800

    fix: 修复加班计时错误

--- src/routes/(admin)/home/+page.svelte
+++ src/routes/(admin)/home/+page.svelte
@@ -31,9 +31,7 @@
 		const users = await res.json();
 
 		const baseTime = new Date('2023-11-26 17:30:00');
-
 		tableData = users.data.map((user) => {
-			debugger;
 			let day = new Date(user.workDate).getDay();
 			let dayLabel = '';
 			if (day == 0) {
@@ -55,28 +53,58 @@
 			// 如果不足30分钟，省去这些时间
 			let endTimeAfter = '';
 			let overtime = 0;
+			let overtime30 = 0;
 			let base = 0;
 			let overtimeAfter = 0;
+			let offset = 30;
 
 			if (user.endTime) {
-				let m = '00';
-				const [hour, minutes] = user.endTime.split(':');
-				if (+minutes < 30) {
-					m = '00';
-				} else if (+minutes < 60) {
-					m = '30';
+				debugger;
+				if (day === 0 || day === 6) {
+					base = 2;
+
+					endTimeAfter = user.endTime;
+					// 计算2个时间的差
+					const end = new Date(`2023-11-26 ${endTimeAfter}:00`);
+					const start = new Date(`2023-11-26 ${user.startTime}:00`);
+					overtime = (end - start) / 60000;
+					//  周末不用偏移
+					overtime30 = overtime;
+				} else {
+					base = 1.5;
+
+					let m = '00';
+					const [hour, minutes] = user.endTime.split(':');
+					if (+minutes < 30) {
+						m = '00';
+					} else if (+minutes < 60) {
+						m = '30';
+					}
+					endTimeAfter = `${hour}:${m}`;
+					if (endTimeAfter === user.endTime) {
+						overtime = 0;
+						overtime30 = 0;
+					} else {
+						// 计算2个时间的差
+						const end = new Date(`2023-11-26 ${endTimeAfter}:00`);
+						overtime = (end - baseTime) / 60000;
+						if (overtime && offset) {
+							overtime30 = overtime - offset;
+						}
+					}
 				}
-				endTimeAfter = `${hour}:${m}`;
 
-				// 计算2个时间的差
-				const end = new Date(`2023-11-26 ${endTimeAfter}:00`);
-				overtime = (end - baseTime) / 6000;
-
-				// 加班基数 = 1.5 或 2
-				base = day === 0 || day === 6 ? 2 : 1.5;
+				// 偶尔会提前下班，公司允许
+				if (overtime < 0) {
+					overtime = 0;
+					overtime30 = 0;
+				}
+				if (overtime30 < 0) {
+					overtime30 = 0;
+				}
 
 				// 加班时长 = 加班基数 * 加班时间
-				overtimeAfter = overtime * base;
+				overtimeAfter = overtime30 * base;
 				// 累加加班时长
 				sumOvertime += overtimeAfter;
 			}
@@ -86,6 +114,7 @@
 				dayLabel,
 				endTimeAfter,
 				overtime,
+				overtime30,
 				base,
 				overtimeAfter
 			};
@@ -105,11 +134,11 @@
 		<TableHeadCell>开始时间</TableHeadCell>
 		<TableHeadCell>结束时间</TableHeadCell>
 		<TableHeadCell>结束时间(调整后)</TableHeadCell>
-		<TableHeadCell>加班时间</TableHeadCell>
 		<TableHeadCell>加班基数</TableHeadCell>
-		<TableHeadCell>加班时长(调整后)</TableHeadCell>
+		<TableHeadCell>加班时间</TableHeadCell>
 		<TableHeadCell>加班偏移量(-30分钟)</TableHeadCell>
-		<TableHeadCell>加班偏移量(-1小时)</TableHeadCell>
+		<!-- <TableHeadCell>加班偏移量(-1小时)</TableHeadCell> -->
+		<TableHeadCell>加班时长(调整后)</TableHeadCell>
 		<TableHeadCell>调休天数</TableHeadCell>
 	</TableHead>
 	<TableBody class="divide-y">
@@ -121,23 +150,24 @@
 				<TableBodyCell>{item.startTime}</TableBodyCell>
 				<TableBodyCell>{item.endTime}</TableBodyCell>
 				<TableBodyCell>{item.endTimeAfter}</TableBodyCell>
-				<TableBodyCell>{item.overtime}</TableBodyCell>
 				<TableBodyCell>{item.base}</TableBodyCell>
+				<TableBodyCell>{item.overtime}</TableBodyCell>
+				<TableBodyCell>{item.overtime30}</TableBodyCell>
+				<!-- <TableBodyCell>{item.overtime > 30 ? item.overtime - 60 : 0}</TableBodyCell> -->
 				<TableBodyCell>{item.overtimeAfter}</TableBodyCell>
-				<TableBodyCell>{item.workOffset1}</TableBodyCell>
-				<TableBodyCell>{item.workOffset1}</TableBodyCell>
-				<TableBodyCell>{item.restDays}</TableBodyCell>
+				<TableBodyCell>{item.rest}</TableBodyCell>
 			</TableBodyRow>
 		{/each}
 		<TableBodyRow>
 			<TableBodyCell>合计</TableBodyCell>
-			<TableBodyCell></TableBodyCell>
-			<TableBodyCell></TableBodyCell>
-			<TableBodyCell></TableBodyCell>
-			<TableBodyCell></TableBodyCell>
-			<TableBodyCell></TableBodyCell>
-			<TableBodyCell></TableBodyCell>
-			<TableBodyCell></TableBodyCell>
+			<TableBodyCell />
+			<TableBodyCell />
+			<TableBodyCell />
+			<TableBodyCell />
+			<TableBodyCell />
+			<TableBodyCell />
+			<TableBodyCell />
+			<TableBodyCell />
 			<TableBodyCell>{sumOvertime}</TableBodyCell>
 		</TableBodyRow>
 	</TableBody>
